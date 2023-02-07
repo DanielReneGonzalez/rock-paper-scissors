a0cf285826481faa2222c76c4644fa00c341958a
Daniel Gonzalez <dgonzaleztx1990@gmail.com>
2023-02-07 08:06:17 -0600

    edit loop so that the game end when the computer hits 5 wins as well, added a please wait message before the game starts.

--- external.js
+++ external.js
@@ -100,7 +100,7 @@
 //runs game a total of five rounds//
 
     function game() {
-            for (playerScore = 0; playerScore < 5;){
+            for (playerScore = 0; playerScore < 5 && computerScore < 5;){
                 console.log('------------------')
                 console.log('Player Score', playerScore)
                 console.log('Computer Score', computerScore)
@@ -109,7 +109,7 @@
              } if (playerScore >= computerScore){
                 console.log('You beat the computer!')
             } else {
-                console.log('You lose! \n Computers are taking over the world!')
+                console.log('You lost! \n Computers are taking over the world!')
             }
     } 
     
@@ -117,6 +117,6 @@
     alert('Open your console to play!')
     const myTimeout = setTimeout(game, 7000)
     console.log('Welcome to a show down of Rock, Paper, or Scissors!')
-    
+    console.log('Please wait...')
 
 
